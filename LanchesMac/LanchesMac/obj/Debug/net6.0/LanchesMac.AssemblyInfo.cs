fadd6ceeba1dce129c91b34c4abd2eb5b74c3bf5
Alessandro Figueiredo <figueiredosoftware@gmail.com>
2024-07-22 21:38:12 -0300

    22/07/2024 21:38
    22/07/2024 21:38

--- LanchesMac/LanchesMac/obj/Debug/net6.0/LanchesMac.AssemblyInfo.cs
+++ LanchesMac/LanchesMac/obj/Debug/net6.0/LanchesMac.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+536282c2d7e74cca99a411e17c05a301d3f85138")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86f5356011be2a278709888728f105ccdb171783")]
 [assembly: System.Reflection.AssemblyProductAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
